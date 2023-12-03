638a8f553108a54eafb946928de30c3b14e4401e
Shadman Uddin <cshadx@gmail.com>
2023-12-03 11:18:37 -0500

    Time: 54 ms (39.85%) | Memory: 17.8 MB (22.44%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = "".join(filter(lambda x: x.isalnum(), s)).lower()
-        
-        if len(s) == 0 or len(s) == 1:
-            return True
     
         left = 0
         right = len(s) - 1
