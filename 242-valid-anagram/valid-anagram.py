b21f5cd6ab4ca8d61e6d74a3027f5ea8a6257454
Shadman Uddin <cshadx@gmail.com>
2023-12-30 22:35:22 -0500

    Time: 100 ms (5.30%) | Memory: 17.8 MB (7.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,8 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        sTimes = {}
-        tTimes = {}
+        sTimes, tTimes = {}, {}
         for i in range(len(s)):
             sTimes[s[i]] = 1 + sTimes.get(s[i], 0)
             tTimes[t[i]] = 1 + tTimes.get(t[i], 0)
