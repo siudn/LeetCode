f7a0e63ae79e697b3487d24fb637e8268faff038
Shadman Uddin <cshadx@gmail.com>
2024-03-22 21:26:51 -0400

    Time: 705 ms (57.50%) | Memory: 20.8 MB (50.17%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,11 +3,13 @@ class Solution:
         result = []
         nums.sort()
 
-        for i in range(len(nums)):
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             j = i + 1
             k = len(nums) - 1
+
             while j < k:
                 sum = nums[i] + nums[j] + nums[k]
                 if sum < 0:
