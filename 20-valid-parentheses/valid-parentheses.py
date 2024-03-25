65970a8196752b6e97ba756e51e6509461f5191e
Shadman Uddin <cshadx@gmail.com>
2024-03-25 14:04:51 -0400

    Time: 38 ms (39.01%) | Memory: 16.7 MB (6.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -8,23 +8,20 @@ class Solution:
             if char == '(' or char == '{' or char == '[':
                 stack.append(char)
             elif char == ')':
-                if len(stack) >= 1 and stack[-1] == '(':
+                if stack and stack[-1] == '(':
                     stack.pop()
                 else:
                     return False
             elif char == '}':
-                if len(stack) >= 1 and stack[-1] == '{':
+                if stack and stack[-1] == '{':
                     stack.pop()
                 else:
                     return False
             else:
-                if len(stack) >= 1 and stack[-1] == '[':
+                if stack and stack[-1] == '[':
                     stack.pop()
                 else:
                     return False
         
-        if len(stack) == 0:
-            return True
-        
-        return False
+        return not stack
         
